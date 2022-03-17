21ebe8e38fd1d8f04c544f15efb791355abd81d3
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-03-17 06:46:48 +0200

    Add some thing new

---
+++ Mortgage.java
@@ -0,0 +1,20 @@
+import java.text.NumberFormat;
+import java.util.Scanner;
+
+public class Mortgage {
+    public static void main (String [] args) {
+//      I don't know the Operation is correct or not by the thing that interest me the the code is correct
+        Scanner Name =new Scanner(System.in);
+        System.out.print("Principal : ");
+        double principal=Name.nextDouble();
+        System.out.print("Annual Interested Rated : ");
+        double Annual=Name.nextDouble();
+        System.out.print("Period(Years) : ");
+        double period=Name.nextDouble();
+        NumberFormat Currancy = NumberFormat.getCurrencyInstance();
+        double result1=(principal*( Annual * Math.pow((1+Annual),period))/(Math.pow((1+Annual),period)-1));
+        String results = Currancy.format((double) principal / Annual);
+        System.out.print("Mortgage" +results);
+
+    }
+}
