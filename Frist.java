06891c8c7e5ea9c5b675c2fa4d9f2a4b3431e110
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2022-03-17 06:43:21 +0200

    Finish Frist work with Java

--- Frist.java
+++ Frist.java
@@ -1,6 +1,20 @@
-public class Frist {
-    public static void main (String [] args){
+import java.text.NumberFormat;
+import java.util.Scanner;
 
-        System.out.println("afjalf");
+public class Frist {
+    public static void main (String [] args) {
+//      I don't know the Operation is correct or not by the thing that interest me the the code is correct
+        Scanner Name =new Scanner(System.in);
+        System.out.print("Principal : ");
+        double principal=Name.nextDouble();
+        System.out.print("Annual Interested Rated : ");
+        double Annual=Name.nextDouble();
+        System.out.print("Period(Years) : ");
+        double period=Name.nextDouble();
+        NumberFormat Currancy = NumberFormat.getCurrencyInstance();
+        double result1=(principal*( Annual * Math.pow((1+Annual),period))/(Math.pow((1+Annual),period)-1));
+        String results = Currancy.format((double) principal / Annual);
+        System.out.print("Mortgage" +results);
+
     }
 }
